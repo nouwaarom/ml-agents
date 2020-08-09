f522a99078b9b1f4d755cda0adee0f03455062a5
Elbert van de Put <elbert@vention.nl>
2020-08-09 16:09:20 +0200

    vehicle: Add semi-legit physics to the vehicle controller.

--- UnitySDK/Assets/ML-Agents/Examples/Traffic/InputManager.cs
+++ UnitySDK/Assets/ML-Agents/Examples/Traffic/InputManager.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    CarController car;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        car = GetComponent<CarController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W)) {
+            car.velocity += 3.0f * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.S)) {
+            if (car.velocity > 0.0f) {
+                car.velocity -= 20.0f * Time.deltaTime;
+            } else {
+                car.velocity -= 2.0f * Time.deltaTime;
+            }
+        } else {
+            if (car.velocity > 0.0f) {
+                car.velocity = Mathf.Max(0.0f, car.velocity - 1.0f * Time.deltaTime);
+            } else if (car.velocity < 0.0f) {
+                car.velocity = Mathf.Min(0.0f, car.velocity + 1.0f * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            car.steer_angle -= 0.5f * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.D)) {
+            car.steer_angle += 0.5f * Time.deltaTime;
+        } else {
+            if (car.steer_angle > 0.1f) {
+                car.steer_angle -= 1.2f * Time.deltaTime;
+            } else if (car.steer_angle < -0.1f) {
+                car.steer_angle += 1.2f * Time.deltaTime;
+            } else {
+                car.steer_angle = 0.0f;
+            }
+        }
     }
 }
