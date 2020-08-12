2ed6fa41016158cd859431ec466e90486fdc3597
Elbert van de Put <elbert@vention.nl>
2020-08-12 13:50:09 +0200

    traffic: Implement DrivingAgent.

--- UnitySDK/Assets/ML-Agents/Examples/Traffic/DrivingAgent.cs
+++ UnitySDK/Assets/ML-Agents/Examples/Traffic/DrivingAgent.cs
@@ -5,22 +5,79 @@ using MLAgents;
 
 public class DrivingAgent : Agent
 {
-    // Generate a random goal position.
+    private RouteProgressRater _progressRater;
+    private VisionRays _vision;
+    CarController _car;
+    
     public override void InitializeAgent()
     {
+        _progressRater = GetComponent<RouteProgressRater>();
+        _vision = GetComponent<VisionRays>();
+        _car = GetComponent<CarController>();
     }
+    
     public override void AgentReset()
     {
+        // We start back at the spawn.
+        transform.position = _progressRater.start.transform.position + new Vector3(0, 2, 0);
+        _car.velocity = 0.0f;
+        _car.steer_angle = 0.0f;
     }
 
     // Check if we collide with something.
     // Check progress towards the goal.
     public override void CollectObservations()
     {
+        AddVectorObs(_vision.distances);
+        AddVectorObs(_car.velocity);
+        AddVectorObs(_car.steer_angle);
     }
 
     public override void AgentAction(float[] vectorAction)
     {
+        var gas = (int)vectorAction[0];
+        switch (gas) {
+            case 0:
+                _car.gasPedalPressed = false;
+                _car.brakePedalPressed = false;
+                break;
+            case 1:
+                _car.gasPedalPressed = true;
+                _car.brakePedalPressed = false;
+                break;
+            case 2:
+                _car.gasPedalPressed = false;
+                _car.brakePedalPressed = true;
+                break;
+        }
+        
+        var steering = (int)vectorAction[0];
+        switch (steering) {
+            case 0:
+                _car.steerLeft = false;
+                _car.steerRight = false;
+                break;
+            case 1:
+                _car.steerLeft = true;
+                _car.steerRight = false;
+                break;
+            case 2:
+                _car.steerLeft = false;
+                _car.steerRight = true;
+                break;
+        }
+        
+        // Add reward and penalties.
+        AddReward(_progressRater.progress);
+        AddReward(-_vision.penalty); // Collisions have a negative reward.
+
+        if (_progressRater.progress > 0.98f) {
+            Done();
+        }
+    }
+
+    public override float[] Heuristic() {
+        return new float[] { 0 };
     }
 
     public void SetResetParameters()
--- UnitySDK/Assets/ML-Agents/Examples/Traffic/RouteProgressRater.cs
+++ UnitySDK/Assets/ML-Agents/Examples/Traffic/RouteProgressRater.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RouteProgressRater : MonoBehaviour
 {
+    public GameObject start;
     private GameObject goal;
     private float initialDistance;
 
@@ -12,6 +13,7 @@ public class RouteProgressRater : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        start = GameObject.FindWithTag("Respawn");     
         goal = GameObject.FindWithTag("Finish");     
         initialDistance = Vector3.Distance(transform.position, goal.transform.position);
     }
