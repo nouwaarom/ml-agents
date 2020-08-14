7f49c943c8a1b7f9fc350cd6ddabf99dd682c8b4
Elbert van de Put <elbert@vention.nl>
2020-08-14 18:19:39 +0200

    misc: Update to new project structure.

---
+++ Project/Assets/ML-Agents/Examples/Traffic/CarController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float velocity = 0.0f;
+    public float steer_angle = 0.0f;
+
+    private float vehicle_length;
+
+    public bool gasPedalPressed = false;
+    public bool brakePedalPressed = false;
+    public bool steerLeft = false; 
+    public bool steerRight = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vehicle_length = GetComponent<Collider>().bounds.size.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update velocity and steer angle based on input.
+        if (gasPedalPressed) {
+            velocity += 3.0f * Time.deltaTime;
+        } else if (brakePedalPressed) {
+            if (velocity > 0.0f) {
+                velocity -= 20.0f * Time.deltaTime;
+            } else {
+                velocity -= 2.0f * Time.deltaTime;
+            }
+        } else {
+            if (velocity > 0.0f) {
+                velocity = Mathf.Max(0.0f, velocity - 1.0f * Time.deltaTime);
+            } else if (velocity < 0.0f) {
+                velocity = Mathf.Min(0.0f, velocity + 1.0f * Time.deltaTime);
+            }
+        }
+        if (velocity > 15) {
+            velocity = 15;
+        } else if (velocity < -5) {
+            velocity = -5;
+        }
+
+        if (steerLeft) {
+            steer_angle -= 0.5f * Time.deltaTime;
+        } else if (steerRight) {
+            steer_angle += 0.5f * Time.deltaTime;
+        } else {
+            if (steer_angle > 0.1f) {
+                steer_angle -= 1.2f * Time.deltaTime;
+            } else if (steer_angle < -0.1f) {
+                steer_angle += 1.2f * Time.deltaTime;
+            } else {
+                steer_angle = 0.0f;
+            }
+        }
+        if (steer_angle > 0.7f * Mathf.PI) {
+            steer_angle = 0.7f * Mathf.PI;
+        }
+        if (steer_angle < -0.7f * Mathf.PI) {
+            steer_angle = -0.7f * Mathf.PI;
+        }
+
+        // Update position based on velocity and steer angle.
+        Vector3 forward = (Quaternion.Euler(0, 90, 0) * transform.forward).normalized;
+
+        // Update the forward position.
+        float forward_velocity = Time.deltaTime * velocity * Mathf.Cos(steer_angle);
+        transform.position += forward * forward_velocity;
+
+        // Update the sidewards position.
+        Vector3 x_direction = (Quaternion.Euler(0, 90, 0) * forward.normalized).normalized;
+        transform.position +=  x_direction * (Time.deltaTime * 0.5f * velocity * Mathf.Sin(steer_angle));
+
+        // Update the rotation.
+        float y_rotation = Mathf.Atan((Time.deltaTime * velocity * Mathf.Sin(steer_angle) / vehicle_length));
+        transform.Rotate(0, Mathf.Rad2Deg * y_rotation, 0);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        velocity = Math.Max(velocity, 1.0f);
+    }
+}
--- Project/Assets/ML-Agents/Examples/Traffic/DrivingAgent.cs
+++ Project/Assets/ML-Agents/Examples/Traffic/DrivingAgent.cs
@@ -1,53 +1,83 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MLAgents;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
 
 public class DrivingAgent : Agent
 {
     private RouteProgressRater _progressRater;
     private VisionRays _vision;
     CarController _car;
+
+    public float timeBetweenDecisionsAtInference;
+    float m_TimeSinceDecision;
     
-    public override void InitializeAgent()
+    public override void Initialize()
     {
+        Debug.Log("Agent initializing.");
         _progressRater = GetComponent<RouteProgressRater>();
         _vision = GetComponent<VisionRays>();
         _car = GetComponent<CarController>();
     }
     
-    public override void AgentReset()
+    public override void OnEpisodeBegin()
     {
+        Debug.Log("Starting episode.");
         // We start back at the spawn.
         transform.position = _progressRater.start.transform.position + new Vector3(0, 2, 0);
+        transform.rotation = _progressRater.start.transform.rotation;
         _car.velocity = 0.0f;
         _car.steer_angle = 0.0f;
+        
+        SetResetParameters();
     }
 
     // Check if we collide with something.
     // Check progress towards the goal.
-    public override void CollectObservations()
+    public override void CollectObservations(VectorSensor sensor)
     {
-        AddVectorObs(_vision.distances);
-        AddVectorObs(_car.velocity);
-        AddVectorObs(_car.steer_angle);
+        sensor.AddObservation(_vision.distances);
+        sensor.AddObservation(_car.velocity);
+        sensor.AddObservation(_car.steer_angle);
     }
 
-    public override void AgentAction(float[] vectorAction)
+    private void FixedUpdate()
+    {
+        if (Academy.Instance.IsCommunicatorOn)
+        {
+            RequestDecision();
+        }
+        else
+        {
+            if (m_TimeSinceDecision >= timeBetweenDecisionsAtInference)
+            {
+                m_TimeSinceDecision = 0f;
+                RequestDecision();
+            }
+            else
+            {
+                m_TimeSinceDecision += Time.fixedDeltaTime;
+            }
+        }
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
     {
         var gas = (int)vectorAction[0];
         switch (gas) {
-            case 0:
+            case 1:
                 _car.gasPedalPressed = false;
                 _car.brakePedalPressed = false;
                 break;
-            case 1:
+            case 0:
                 _car.gasPedalPressed = true;
                 _car.brakePedalPressed = false;
                 break;
             case 2:
                 _car.gasPedalPressed = false;
-                _car.brakePedalPressed = true;
+                _car.brakePedalPressed = false; // TODO, make true so that is learns to go forward as well.
                 break;
         }
         
@@ -68,16 +98,20 @@ public class DrivingAgent : Agent
         }
         
         // Add reward and penalties.
-        AddReward(_progressRater.progress);
+        AddReward(5.0f * _progressRater.progress);
         AddReward(-_vision.penalty); // Collisions have a negative reward.
 
         if (_progressRater.progress > 0.98f) {
-            Done();
+            EndEpisode(); // Finished.
+        } else if (_progressRater.progress < -0.1f) { // Hard fail, if we drive back.
+            AddReward(-1000.0f);
+            EndEpisode();
         }
     }
 
-    public override float[] Heuristic() {
-        return new float[] { 0 };
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = 0;
     }
 
     public void SetResetParameters()
